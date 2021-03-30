cf850f7813bd870fe620cdaeec53ffe37af9c66f
Johanna Benndorf <johanna.benndorf@gmail.com>
2021-03-30 19:32:33 +0100

    ProductController returns Twig templates modified with bootstrap

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +15,26 @@ class ProductController extends AbstractController
     /**
      * @Route("/products", name="app_products")
      */
-    public function displayProducts()
+    public function displayProducts(EntityManagerInterface $em)
     {
-        return new Response('This page will display all store products');
+        $repository = $em -> getRepository(Product::class);
+        $products = $repository -> findAll();
+       return $this->render('products.html.twig', [
+           'products' => $products
+       ]);
+    }
+
+    /**
+     * @Route("/products/{title}", name="app_product/{title}")
+     * @param $title
+     * @return Response
+     */
+    public function displayProductByTitle($title, EntityManagerInterface $em)
+    {
+        $repository = $em -> getRepository(Product::class);
+        $product = $repository -> findOneBy(['title' => $title]);
+        return $this->render('products.html.twig', [
+            'products' => [$product]
+        ]);
     }
 }

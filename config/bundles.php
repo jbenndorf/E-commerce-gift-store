1d6616a391bce6b34e9587469ab4db17817b7111
Johanna Benndorf <johanna.benndorf@gmail.com>
2021-04-01 14:54:10 +0100

    Addition of Profiler service and Product caching

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,15 +9,26 @@ use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class ProductController extends AbstractController
 {
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em){
+        $this -> em = $em;
+    }
+
     /**
      * @Route("/products", name="app_products")
      */
-    public function displayProducts(EntityManagerInterface $em)
+    public function displayProducts()
     {
-        $repository = $em -> getRepository(Product::class);
+        $repository = $this -> em -> getRepository(Product::class);
         $products = $repository -> findAll();
        return $this->render('products.html.twig', [
            'products' => $products
@@ -29,10 +40,12 @@ class ProductController extends AbstractController
      * @param $title
      * @return Response
      */
-    public function displayProductByTitle($title, EntityManagerInterface $em)
+    public function displayProductByTitle($title, CacheInterface $cache)
     {
-        $repository = $em -> getRepository(Product::class);
-        $product = $repository -> findOneBy(['title' => $title]);
+        $repository = $this -> em -> getRepository(Product::class);
+        $product = $cache -> get($title, function() use ($repository, $title) {
+            return $repository -> findOneBy(['title' => $title]);
+        });
         return $this->render('products.html.twig', [
             'products' => [$product]
         ]);
